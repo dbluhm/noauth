a7ccd7396aa8dc2c15e07bc5591513c5f1f89a40
Daniel Bluhm <dbluhm@pm.me>
2024-07-02 22:17:18 -0400

    fix: healthcheck
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- healthcheck.py
+++ healthcheck.py
@@ -4,5 +4,10 @@
 import socket
 import sys
 
+if len(sys.argv) < 3:
+    print("USAGE: python healthcheck.py <HOST> <PORT>")
+
+host = sys.argv[1]
+port = int(sys.argv[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sys.exit(s.connect_ex(("localhost", 8080)))
+sys.exit(s.connect_ex((host, port)))
